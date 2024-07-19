835a397f1dc811dd9003ceb229f1c286d7578f36
AnuragRaj11 <Araj59519@gmail.com>
2024-07-20 01:00:22 +0530

    var

--- var_DT.py
+++ var_DT.py
@@ -3,3 +3,17 @@ b=1
 c="an"
 d=2.2
 print(a+b+d)
+
+com=2==2
+print(com)
+
+t=type(a)
+print(t)
+
+
+inn=int(input("enter no 1:"))
+ip=int(input("enter no 2:"))
+
+print(inn)
+print(ip)
+print("sum id ",inn+ip)

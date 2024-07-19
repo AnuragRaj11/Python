44093e7436b3535be8df4dcb8481d35e4fab1d5d
AnuragRaj11 <Araj59519@gmail.com>
2024-07-20 00:07:13 +0530

    new

--- modules.py
+++ modules.py
@@ -3,10 +3,10 @@ import pyjokes
 print("hello py")
 
 joke=pyjokes.get_joke()
-print("joke")
+print("joke-python")
 
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("I will speak this text")
+engine.say("hello python master")
 engine.runAndWait()
 

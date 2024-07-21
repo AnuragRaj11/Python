b4271b34f3eb26c14616410d4cc5220d43364e82
AnuragRaj11 <Araj59519@gmail.com>
2024-07-22 00:00:29 +0530

    loops

--- loops.py
+++ loops.py
@@ -1,6 +1,15 @@
 i=1
 while (i<51):
+    pass
     print(i)
-    # i=i+1  
-    i+=1
+    i=i+1  
+    # i+=1
+    break
+
+    a=5
+    for a in range(5):
+        print(a)
+        continue
     
+    else:
+        print("done")

eeab7f4d5c7b19fdbe9773233d5f70e374a7e352
AnuragRaj11 <Araj59519@gmail.com>
2024-07-24 00:23:55 +0530

    proj2

---
+++ Project2.py
@@ -0,0 +1,17 @@
+import random
+n=random.randint(1,100)
+a=-1
+gusses=1
+
+while a!=n:
+ a=int(input("enter the number:"))
+ if(a>n):
+     print("too low")
+     gusses=+1
+ elif(a<n):
+     print("too high")
+     gusses=+1
+ 
+ 
+ print("you have gussed the no {n} correctly in {gusses} attempts")    
+     
